2964e010b43ba9f36dc6e713c24de6b673f289b1
mengxin <19868847@qq.com>
2018-05-18 16:41:05 +0800

    create module

--- algorithm/src/main/java/HelloWord.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 冯刚 on 2018/5/15.
- */
-public class HelloWord {
-
-    public static void main(String[] arrg){
-        System.out.print("Hello Word!");
-    }
-}
---
+++ pipe/src/main/java/com/deepthoughtdata/PipeApplication.java
@@ -0,0 +1,17 @@
+package com.deepthoughtdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PipeApplication
+ *
+ */
+@SpringBootApplication
+public class PipeApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(PipeApplication.class, args);
+    }
+}
