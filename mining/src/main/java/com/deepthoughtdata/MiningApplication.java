61996916756135928a8dfb74c13e43567f7c72ad
hanwei36 <13051111332@163.com>
2018-07-01 11:43:56 +0800

    pipe 和 mining 集成sidecar

--- mining/src/main/java/com/deepthoughtdata/MiningApplication.java
+++ mining/src/main/java/com/deepthoughtdata/MiningApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSidecar
 @EnableSwagger2
 public class MiningApplication
 {
--- pipe/src/main/java/com/deepthoughtdata/PipeApplication.java
+++ pipe/src/main/java/com/deepthoughtdata/PipeApplication.java
@@ -1,17 +1,34 @@
 package com.deepthoughtdata;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * PipeApplication
- *
  */
 @SpringBootApplication
-public class PipeApplication
-{
-    public static void main( String[] args )
-    {
+@EnableEurekaClient
+@EnableSidecar
+@EnableSwagger2
+public class PipeApplication {
+
+    @Autowired
+    private RestTemplateBuilder builder;
+
+    // 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例
+    @Bean
+    public RestTemplate restTemplate() {
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(PipeApplication.class, args);
     }
 }
