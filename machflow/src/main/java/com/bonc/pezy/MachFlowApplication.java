89799ba0ade4be108810a059741285a87ba5cb33
fenggang <fg191392@163.com>
2018-07-02 16:13:53 +0800

    添加工作流服务

--- machflow/src/main/java/com/bonc/pezy/MachFlowApplication.java
+++ machflow/src/main/java/com/bonc/pezy/MachFlowApplication.java
@@ -2,8 +2,12 @@ package com.bonc.pezy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class MachFlowApplication {
 
 	public static void main(String[] args) {
