d8559bd5f83d1c33cb3052ac4d9aef9f2eef1338
machen990519 <493137170@qq.com>
2018-07-06 10:37:35 +0800

    修改上传文件返回的数据

--- dataset/src/main/java/com/dataset/management/rest/DataSetSystemController.java
+++ dataset/src/main/java/com/dataset/management/rest/DataSetSystemController.java
@@ -100,7 +100,6 @@ public class DataSetSystemController {
         newDataSystem.setDataSetId(datasetId);
         //体统表信息
         logger.info("数据集系统表开始创建：。。。。。");
-        dataSetOptService.save(newDataSystem);
         return  ResultUtil.success(dataSetOptService.save(newDataSystem));
     }
     //查询
