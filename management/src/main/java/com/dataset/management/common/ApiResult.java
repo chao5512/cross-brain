74ad3b27f2a2d0ec9af91309888c75039614003c
wangpeiwen <984972185@qq.com>
2018-07-09 12:32:34 +0800

    管理

---
+++ management/src/main/java/com/dataset/management/common/ApiResult.java
@@ -0,0 +1,56 @@
+package com.dataset.management.common;
+
+/**
+ * API 返回结果类
+ */
+public class ApiResult {
+
+    private int code;
+    private Object data;
+    private String message;
+
+    public ApiResult(int code, Object data, String message) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+    public ApiResult(int code,String message){
+        this.code =code;
+        this.message =message;
+    }
+
+    public int getcode() {
+        return code;
+    }
+
+    public void setResult(int code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", msg='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
