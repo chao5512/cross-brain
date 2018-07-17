c48b7b96458aaa04081bc6a71e42f4c38b31f7f2
machen990519 <493137170@qq.com>
2018-07-17 10:10:08 +0800

    添加创建时，选择数据集权限的操作

--- dataset/src/main/java/com/dataset/management/rest/DataSetSystemController.java
+++ dataset/src/main/java/com/dataset/management/rest/DataSetSystemController.java
@@ -57,15 +57,17 @@ public class DataSetSystemController {
     private static final ExecutorService exeService = Executors.newFixedThreadPool(5);
 
     @ResponseBody
-    @RequestMapping(value = "/create/{dataSetName}/{userId}/{dataSetDesc}",method = RequestMethod.POST)
+    @RequestMapping(value = "/create/{dataSetName}/{userId}/{dataSetDesc}/{dataSetPower}",method = RequestMethod.POST)
     public ApiResult createDataSet(@PathVariable(value = "dataSetName") String dataSetName,
                                    @PathVariable(value = "dataSetDesc") String datSetDesc,
+                                   @PathVariable(value = "dataSetPower") String dataSetPower,
                                    @PathVariable(value = "userId") int userId) throws IOException{
         //先生成默认的
         DataSet dataSet = packageDataSet();
         dataSet.setDataSetName(dataSetName);
         dataSet.setUserId(userId);
         dataSet.setDataSetBasicDesc(datSetDesc);
+        dataSet.setDataSetPower(dataSetPower);
         dataSet.setDataSetHiveTableName(null);
         dataSet.setDataSetHiveTableId(null);
         dataSet.setDataSetEnglishName(null);
