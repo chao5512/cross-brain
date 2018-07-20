80f41efca8c9534629e51f77dff54009fb4b1d4f
wangpeiwen <984972185@qq.com>
2018-07-20 14:56:00 +0800

    hive配置信息读取方式更改

--- mining/src/main/python/hiveutil.py
+++ mining/src/main/python/hiveutil.py
@@ -1,38 +1,62 @@
 from sqlalchemy import *
 import pandas as pd
+import configparser
+
 
 class HiveClient:
-    engine=None
+    engine = None
+
     def __init__(self):
         super(HiveClient, self).__init__()
+
     @staticmethod
     def getEngine(database):
         """
+        从配置文件中读取配置
+        """
+        cf = configparser.ConfigParser()
+        cf.read("../resources/ini.cfg")
+        """
         创建 engine
         """
-        HiveClient.engine = create_engine('hive://182.92.82.3:10000/%s'%(database))
+        url = "hive://{}:{}/%s"
+        url = url.format(cf.get("hive","db_host"),cf.get("hive","port"))
+        HiveClient.engine = create_engine(url % (database))
         return HiveClient.engine
+
     @staticmethod
-    def queryByRowNums(tablename,rownums=1000,database="default"):
+    def queryByRowNums(tablename, rownums=1000, database="default"):
         engine = HiveClient.getEngine(database=database)
-        sql="select * from %s limit %s"%(tablename,rownums)
-        datas=pd.read_sql(sql, engine,coerce_float=False)
+        sql = "select * from %s limit %s" % (tablename, rownums)
+        datas = pd.read_sql(sql, engine, coerce_float=False)
         return datas
+
     @staticmethod
-    def queryForAll(tablename,database="default"):
+    def queryForAll(tablename, database="default"):
         engine = HiveClient.getEngine(database=database)
-        sql="select * from %s"%(tablename)
-        datas=pd.read_sql(sql, engine)
+        sql = "select * from %s" % (tablename)
+        datas = pd.read_sql(sql, engine)
         return datas
+
     @staticmethod
-    def queryRowNumber(tablename,database="default"):
+    def queryRowNumber(tablename, database="default"):
         engine = HiveClient.getEngine(database=database)
-        sql="select count(*) count from  %s"%(tablename)
-        number=pd.read_sql(sql,engine)
+        sql = "select count(*) count from  %s" % (tablename)
+        number = pd.read_sql(sql, engine)
         return number
 
+
 if __name__ == '__main__':
-    datas = HiveClient.queryByRowNums(tablename="150_titanic",rownums=10)
+    print(HiveClient.queryRowNumber(tablename="150_titanic"))
+    # cf = configparser.ConfigParser()
+    # print(cf.read("../resources/ini.cfg"))
+    # print(cf.get("hive","db_host"))
+    # print(cf.get("hive","port"))
+    # print(cf.get("hive","user"))
+    # str="hive://{}:{}/%s"
+    # str1=str.format(cf.get("hive","db_host"),cf.get("hive","port"))
+    # print(str1)
+# datas = HiveClient.queryByRowNums(tablename="150_titanic", rownums=10)
     #     print(HiveClient.queryRowNumber(tablename="150_titanic"))
     # number = HiveClient.queryRowNumber(tablename='studentno')
     # list=number.head().to_dict(orient='split')['data']
