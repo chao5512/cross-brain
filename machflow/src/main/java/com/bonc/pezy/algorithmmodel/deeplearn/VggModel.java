1ec27a47fbf31d3a56929bc52529b09c4a3bc0d5
fenggang <fg191392@163.com>
2018-07-31 14:32:33 +0800

    修改

--- machflow/src/main/java/com/bonc/pezy/algorithmmodel/deeplearn/VggModel.java
+++ machflow/src/main/java/com/bonc/pezy/algorithmmodel/deeplearn/VggModel.java
@@ -1,6 +1,5 @@
 package com.bonc.pezy.algorithmmodel.deeplearn;
 
-import com.bonc.pezy.dataconfig.DataConfig;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
 
@@ -11,7 +10,6 @@ import java.io.Serializable;
  */
 public class VggModel implements Serializable, ExecutionListener {
 
-    private DataConfig dataConfig = DataConfig.getDataConfig();
     @Override
     public void notify(DelegateExecution execution) throws Exception {
 
