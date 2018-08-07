4189d8b38586ab55cf238907fcb757cde764c95f
mengxin <19868847@qq.com>
2018-08-07 15:30:15 +0800

    update pipeline

--- pipe/src/main/python/util/HDFSUtil.py
+++ pipe/src/main/python/util/HDFSUtil.py
@@ -5,13 +5,14 @@ from config import Config
 
 class HDFSUtil():
     @staticmethod
-    def append(job_path,content):
+    def append(job_path,content,append):
         conf = Config.loadConfig()
         # 连接HDFS集群
         client = Client(conf.get('cluster','hadoopMaster'))
         with client.write(job_path,
-                          overwrite=False,append=False,encoding='utf-8') as writer:
+                          overwrite=False,append=append,encoding='utf-8') as writer:
             writer.write(content)
 
 if __name__ == '__main__':
-    HDFSUtil.append("/test.log","test")
+    HDFSUtil.append("/test.log","",False)
+    HDFSUtil.append("/test.log","123",True)
