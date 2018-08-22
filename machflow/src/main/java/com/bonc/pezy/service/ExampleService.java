4c23b3ac78a05c73958f090b3f42b1fc1b621ee0
945918259@qq.com <bgp123123>
2018-08-23 02:18:58 +0800

    添加通过example创建model接口

--- machflow/src/main/java/com/bonc/pezy/service/ExampleService.java
+++ machflow/src/main/java/com/bonc/pezy/service/ExampleService.java
@@ -1,6 +1,7 @@
 package com.bonc.pezy.service;
 
 import com.bonc.pezy.entity.Example;
+import com.bonc.pezy.entity.Model;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface ExampleService {
     public List<Example> findByModelNameContaining(String exampleName);
     public List<Example> findByExampleType(short exampleType);
     public List<Example> findAll();
+    public Model createModel(String exampleId,Long owner);
 }
